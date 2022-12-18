0421fa002692ba12fb9a596a628006cded02ece6
Максим Комолов <maxim.kmb@gmail.com>
2022-12-18 14:39:50 +0300

    code with type hints

---
+++ temp.py
@@ -0,0 +1,5 @@
+def (remove_rep(current_rep: float, rep_points: int, debuf_effect: bool) -> float:
+    current_rep -= rep_points
+    if debuf_effect:
+        return current_rep * ANTIBONUS
+    return float(current_rep)
